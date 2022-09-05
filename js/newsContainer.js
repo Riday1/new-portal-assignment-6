b2f86c29b7d44bd61fe92fd9e3475ddc65d2cec5
Riday1 <rrriday100@gmail.com>
2022-09-06 02:31:06 +0600

    navbar is added

--- js/newsContainer.js
+++ js/newsContainer.js
@@ -10,9 +10,24 @@ const displayNewCategory = (newsCategories) => {
     newsCategories.forEach(category => {
         const categoryList = document.createElement('li')
         categoryList.classList.add('items')
+        categoryList.setAttribute('onclick', `loadNewsDetails(${category.category_id})`)
         categoryList.innerText = `${category.category_name}`
         newsCategoryContainer.appendChild(categoryList)
+
+
     });
 
 }
+
+const loadCategoriesDetails = (categoryDetails) => {
+    const newsBlogContainer = document.getElementById('news-blog-container')
+    newsBlogContainer.innerHTML = ``;
+    categoryDetails.forEach(blog => {
+        
+    })
+
+
+}
+
+
 newsCategoryContainer()
