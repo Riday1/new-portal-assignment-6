cec38399004f4e119deb307c93316d6fa43a37f6
Riday1 <rrriday100@gmail.com>
2022-09-06 22:04:06 +0600

    footer section is added

--- js/newsContainer.js
+++ js/newsContainer.js
@@ -10,7 +10,7 @@ const displayNewCategory = (newsCategories) => {
     newsCategories.forEach(category => {
         const categoryList = document.createElement('li')
         categoryList.classList.add('items')
-        categoryList.setAttribute('onclick', `loadNewsDetails(${category.category_id})`)
+        categoryList.setAttribute('onclick', `loadCategoriesData(${category.category_id})`)
         categoryList.innerText = `${category.category_name}`
         newsCategoryContainer.appendChild(categoryList)
 
@@ -19,12 +19,62 @@ const displayNewCategory = (newsCategories) => {
 
 }
 
-const loadCategoriesDetails = (categoryDetails) => {
-    const newsBlogContainer = document.getElementById('news-blog-container')
-    newsBlogContainer.innerHTML = ``;
-    categoryDetails.forEach(blog => {
+const loadCategoriesData = (newId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/0${newId}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategroyDetails(data.data))
+}
+
+const displayCategroyDetails = (categriesDetails) => {
+    const newBlogContainers = document.getElementById('news-blog-container')
+    newBlogContainers.innerHTML = ``
+    categriesDetails.forEach(category => {
+        console.log(category)
+        const blogDiv = document.createElement('div')
+        blogDiv.classList.add('mt-3')
+        blogDiv.innerHTML = `
+        <div class="d-flex justify-content-center align-items-center  border rounded-4 p-3 flex-column flex-md-row bg-white shadow-sm">
+            <div class="col-4 ">
+                <img src="${category.thumbnail_url}" class="img-fluid w-100 h-100" alt="">
+            </div>
+            <div class="col-8 ps-2 ">
+                <h2>${category.title}</h2>
+                <p>${category.details.slice(0, 600)}</p>
+                <div class="d-flex justify-content-between           align-items-center">
+                    <div class="col-3 d-flex ">
+                        <img class="w-25  rounded-circle" src="${category.author.img}" alt="">
+                        <div class="ps-2">
+                            <h5>${category.author.name ? category.author.name : 'no data available'}</h5>
+                            <p>${category.author.published_date}</p>
+                        </div>
+                    </div>
+                    <div class="col-3 text-center" >
+                        <i class="fa-regular fa-eye"></i>
+                        <span>${category.total_view ? category.total_view : 'no data avaiable'}</span>
+                    </div>
+                    <div class="col-3 fs-4 text-center">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                    <div class="col-3 text-center">
+                        <button class="btn btn-primary">Show Details <iclass="fa-solid fa-arrow-right-long"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
         
+        `
+
+        newBlogContainers.appendChild(blogDiv)
+
     })
+
 
 
 }
